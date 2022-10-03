aa5f970dd9581a03fc2f0c1d27567ea73ac856f5
Roman Voitalik <voitalik1@gmail.com>
2022-10-03 22:17:25 +0300

    resolve task

--- 1-0-java-basics/1-3-0-hello-generics/src/main/java/com/bobocode/basics/BoxDemoApp.java
+++ 1-0-java-basics/1-3-0-hello-generics/src/main/java/com/bobocode/basics/BoxDemoApp.java
@@ -9,8 +9,8 @@ package com.bobocode.basics;
  */
 public class BoxDemoApp {
     public static void main(String[] args) {
-        Box intBox = new Box(123);
-        Box intBox2 = new Box(321);
+        Box<Integer> intBox = new Box(123);
+        Box<Integer> intBox2 = new Box(321);
         System.out.println((int) intBox.getValue() + (int) intBox2.getValue());
 
         intBox.setValue(222);
